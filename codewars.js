8978b5632054c1de5fc1085469609cb9a604771f
rmagley <rachelmagley@gmail.com>
2022-03-08 11:44:38 -0800

    Complete twoSum

--- codewars.js
+++ codewars.js
@@ -64,16 +64,44 @@
 
 // console.log((areaOrPerimeter(4 , 4)));
 
-const ReverseString = (str) => {
-	let arr = str.split('');
-	let newArr = [];
-	for (let i = arr.length - 1; i > -1; i--) {
-		newArr.push(arr[i]);
-	}
-	// console.log(newArr);
-	let rev = newArr.join('');
-	console.log(rev);
-	return rev;
-};
+// const ReverseString = (str) => {
+// 	let arr = str.split('');
+// 	let newArr = [];
+// 	for (let i = arr.length - 1; i > -1; i--) {
+// 		newArr.push(arr[i]);
+// 	}
+// 	// console.log(newArr);
+// 	let rev = newArr.join('');
+// 	console.log(rev);
+// 	return rev;
+// };
 
-ReverseString('Hello I am Rachel');
+// ReverseString('Hello I am Rachel');
+
+//takes 2 parameters, arr1, arr2
+//join 2 arrays
+//implement bubble sort - While Loop
+	//Sorted = True
+	//start at idx 0, compare to current idx + 1
+	//if current idx < next idx, slide down to the next idx position, Sorted = true
+	//if current idx > next idx, current idx data and move to the next position
+	//slide down to the next idx Sorted = false
+	//As long as sorted is false, continue to loop
+	//Once loop completes and sorted stays true, break loop
+//return the final array
+
+// function mergeSortedArrays(arr1, arr2) {
+// 	let sortedArr = arr1 + arr2;
+// 	console.log(sortedArr);
+// }
+
+
+// mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]);
+
+function twoSum(numbers, target) {
+	for (var i = 0; i < numbers.length - 1; i++) {
+		for (var j = i + 1; j < numbers.length; j++) {
+			if (numbers[i] + numbers[j] === target) return [i, j];
+		}
+	}
+}
